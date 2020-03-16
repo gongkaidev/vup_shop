e59ff92aa2be568cc8bb31c446834da361654fca
gk <1297876624@qq.com>
2020-03-16 15:29:10 +0800

    "修改"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'
 import './plugins/element.js'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 new Vue({
   router,
   render: h => h(App)
