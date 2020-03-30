3474aa0de539d7597d3383785a542ffbe5a4ce95
gk <1297876624@qq.com>
2020-03-30 21:51:26 +0800

    完成数据列表功能

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import Goods from '../components/goods/Goods.vue'
 import GoodsAdd from '../components/goods/GoodsAdd.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -30,7 +31,9 @@ const router = new VueRouter({
         { path: '/params', component: Params },
         { path: '/goods', component: Goods },
         { path: '/goods/add', component: GoodsAdd },
-        { path: '/orders', component: Order }
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
+
       ]
     }
   ]
